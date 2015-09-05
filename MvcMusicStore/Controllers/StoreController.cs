94096bd94fa7d26cd9675b3b5e3f1c7bc5747c63
Nicolas <nico@gmail.com>
2015-09-05 18:43:10 -0300

    Added Home and Store controllers. Addes layout.

---
+++ MvcMusicStore/Controllers/StoreController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMusicStore.Controllers
+{
+    public class StoreController : Controller
+    {
+        //
+        // GET: /Store/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public String Browse(string genre)
+        {
+            return "HELLO BROWSE. GENRE: " + HttpUtility.HtmlEncode(genre);
+        }
+
+        public String Details(int id)
+        {
+            return "HELLO Details. ID: " + id;
+        }
+
+    }
+}
