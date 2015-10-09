ed11db73c6f42625ebeecee7317f76306a587f14
Nicolas <nico@gmail.com>
2015-10-09 19:58:45 -0300

    Ajax web form for Genre creation.

--- MvcMusicStore/Models/Genre.cs
+++ MvcMusicStore/Models/Genre.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace MvcMusicStore.Models
 {
     public class Genre
     {
         public int GenreId { get; set; }
+        [Required (ErrorMessage = "A Name is required")]
+        [StringLength(20, MinimumLength = 2)]
         public String Name { get; set; }
         public string Description { get; set; }
         public List<Album> Albums { get; set; }
---
+++ MvcMusicStore/Scripts/chooseGenre.js
@@ -0,0 +1,72 @@
+﻿/// <reference path="jquery-1.4.4-vsdoc.js" />
+$(function () {
+    $("#genreDialog").dialog({
+        autoOpen: false,
+        width: 480,
+        height: 250,
+        modal: true,
+        title: 'Add Genre',
+        modal: true,
+        buttons: {
+            'Save': function () {
+                var createGenreForm = $("#createGenreForm");
+
+                if (createGenreForm.valid()) {
+                    $.post(createGenreForm.attr('action'), createGenreForm.serialize(), function (data) {
+                        try {
+                            if (data.Error != '' && data.Error != undefined) {
+                                var errorMsg = "";
+                                for (var i = 0; i < data.Error.length; i++) {
+                                    errorMsg += data.Error[i].ErrorMessage + "\n";
+                                }
+                                alert(errorMsg);
+                            }
+                            else {
+                                //Note: prop method is not supported in jquery-1.4.4
+                                $("#GenreId").append(
+                                    $("<option></option>")
+                                    .val(data.Genre.GenreId)
+                                    .html(data.Genre.Name)
+                                    .attr("selected", true)
+                                     );
+
+                                $("#genreDialog").dialog('close');
+                            }
+                        } catch (e) { alert(e); }
+                    });
+                }
+            },
+            'Cancel': function () {
+                $(this).dialog('close');
+            }
+        }
+
+    });
+
+});
+
+$('#genreAddLink').click(function () {
+
+    var createFormUrl = $(this).attr('href');
+
+    $('#genreDialog').html('')
+
+    .load(createFormUrl, function () {
+
+        // The createGenreForm is loaded on the fly using jQuery load. 
+
+        // In order to have client validation working it is necessary to tell the 
+
+        // jQuery.validator to parse the newly added content
+
+        jQuery.validator.unobtrusive.parse('#createGenreForm');
+
+        $('#genreDialog').dialog('open');
+
+    });
+
+
+
+    return false;
+
+});
